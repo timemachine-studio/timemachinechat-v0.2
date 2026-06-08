08fcbe8b38e869f915e28195cb585b61ff840764
Tanzim <flyingprisma0001@gmail.com>
2026-06-08 14:07:19 +0600

    Change model selection for image generation

--- api/image.ts
+++ api/image.ts
@@ -35,7 +35,7 @@ function constructPollinationsUrl(params: ImageParams): URL {
     model = persona === 'girlie' ? 'klein' : 'klein';
   } else {
     // Create process: use seedream/zimage models
-    model = persona === 'girlie' ? 'zimage' : 'zimage';
+    model = persona === 'girlie' ? 'gptimage-large' : 'gptimage-large';
   }
 
   // Use WHATWG URL API to avoid url.parse() deprecation warning
